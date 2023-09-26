bfb8c855b5e91ff64150f5e7798a069a3687b4e0
Mateus Lucas <mateus.some.ms@gmail.com>
2023-09-26 20:16:28 -0300

    terminado de fazer o js, acrescentado classes no html e no css, agora vou fazer o Services

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,6 +59,26 @@ skillsHeader.forEach((el) =>{
 
 /*====================== GUIAS DE QUALIFICAÇÃO ====================*/
 
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('clicl',() =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
 
 /*====================== MODAL DE SERVIÇOS ====================*/
 
