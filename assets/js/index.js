ecc1665d33b17fd693ca51bc7f74e5f9df15231d
Mateus Lucas <mateus.some.ms@gmail.com>
2023-10-01 17:23:57 -0300

    arrumado o js e html da parte de serviços

--- assets/js/index.js
+++ assets/js/index.js
@@ -90,8 +90,8 @@ tabs.forEach(tab => {
 
 /*====================== MODAL DE SERVIÇOS ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('services__button'),
-      modalCloses = document.querySelectorAll('services__modal-close')
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
 let modal = function(modalClick){
     modalViews[modalClick].classList.add('active-modal')
