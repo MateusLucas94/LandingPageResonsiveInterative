33bc960b132fea4b53bdb0f61387df8116911575
Mateus Lucas <mateus.some.ms@gmail.com>
2023-10-02 13:20:04 -0300

    adicionado arquivo swiper.css, swiper.js e imagem portifolio1   feita a parte do portifolio com swiper no HTML,CSS  e JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -115,7 +115,19 @@ modalCloses.forEach((modalClose) =>{
 
 
 /*====================== PORTFOLIO SWIPER ====================*/
+let swiper = new Swiper(".portifolio__container", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
 
 /*====================== DEPOIMENTO ======================*/
 
